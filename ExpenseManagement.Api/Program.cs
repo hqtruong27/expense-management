e7f0574497904f77c9a155610efe2eccf4589660
Truong Hoang <hqtruong27@gmail.com>
2022-02-23 21:06:46 +0700

    add swagger

--- ExpenseManagement.Api/Program.cs
+++ ExpenseManagement.Api/Program.cs
@@ -12,49 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "My API",
-        Version = "v1"
-    });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        Name = "JWT Authentication",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-                {
-                 new OpenApiSecurityScheme
-                 {
-                   Reference = new OpenApiReference
-                   {
-                     Type = ReferenceType.SecurityScheme,
-                     Id = "Bearer"
-                   }
-                  },
-                  Array.Empty<string>()
-                }
-              });
-});
 
-builder.Host.UseSerilog((hc, lc) =>
-{
-    lc.ReadFrom.Configuration(hc.Configuration)
-               .Enrich.FromLogContext()
-               .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name)
-               .Enrich.WithProperty("Environment", hc.HostingEnvironment);
-#if DEBUG
-    // Used to filter out potentially bad data due debugging.
-    // Very useful when doing Seq dashboards and want to remove logs under debugging session.
-    lc.Enrich.WithProperty("DebuggerAttached", System.Diagnostics.Debugger.IsAttached);
-#endif
-});
 
 builder.Register();
 
@@ -77,10 +35,10 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
+app.UseSwaggerExtensions();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-//app.UseSerilogRequestLogging();
+app.UseSerilogRequestLogging();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
