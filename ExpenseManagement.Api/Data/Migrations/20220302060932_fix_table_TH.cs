68973598a4a713768b2a8694954f0d7252916dea
Truong Hoang <hqtruong27@gmail.com>
2022-03-02 13:10:42 +0700

    Fix table TransactionHistory

---
+++ ExpenseManagement.Api/Data/Migrations/20220302060932_fix_table_TH.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseManagement.Api.Data.Migrations
+{
+    public partial class fix_table_TH : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DebtChargeId",
+                table: "TransactionHistories",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DebtChargeId",
+                table: "TransactionHistories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
