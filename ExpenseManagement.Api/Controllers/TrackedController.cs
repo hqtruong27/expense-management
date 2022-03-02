40363328f487fd4207502275197148a004541be2
Truong Hoang <hqtruong27@gmail.com>
2022-03-02 17:53:51 +0700

    tracked controller

---
+++ ExpenseManagement.Api/Controllers/TrackedController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackedController : ControllerBase
+    {
+        protected string? RemoteIpAddress => HttpContext.Connection.RemoteIpAddress?.ToString();
+        protected string? LocalIpAddress => HttpContext.Connection.LocalIpAddress?.ToString();
+        protected int RemotePort => HttpContext.Connection.RemotePort;
+        protected int LocalPort => HttpContext.Connection.LocalPort;
+    }
+}
