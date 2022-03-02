96442042e999cc6f589d5f4cfdd22dc5eac509b0
Truong Hoang <hqtruong27@gmail.com>
2022-03-02 13:09:10 +0700

    new Data Model, sm change new 2 model DebtCharge and transactionHistory, sm change model User

--- ExpenseManagement.Api/Data/Models/DebtCharge.cs
+++ ExpenseManagement.Api/Data/Models/DebtCharge.cs
@@ -1,22 +1,26 @@
-﻿//using ExpenseManagement.Api.Enum;
-//using System.ComponentModel.DataAnnotations;
-//using System.ComponentModel.DataAnnotations.Schema;
+﻿using ExpenseManagement.Api.Data.Models.Base;
+using ExpenseManagement.Api.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-//namespace ExpenseManagement.Api.Data.Models
-//{
-//    public class DebtCharge
-//    {
-//        [Key]
-//        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-//        public int Id { get; set; }
-//        public string Name { get; set; } = string.Empty;
-//        [DataType(DataType.Currency)]
-//        public decimal Amount { get; set; }
-//        public string Description { get; set; } = string.Empty;
-//        public string UserId { get; set; } = string.Empty;
-//        [ForeignKey("UserId")]
-//        public User? User { get; set; }
-//        public DebtChargeType Type { get; set; }
-//        public DebtChargeStatus Status { get; set; }
-//    }
-//}
+namespace ExpenseManagement.Api.Data.Models
+{
+    public class DebtCharge : BaseTrackedEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Amount { get; set; }
+        public string CreditorId { get; set; } = string.Empty;
+        [ForeignKey("CreditorId")]
+        public User? Creditor { get; set; }
+        public string DebtorId { get; set; } = string.Empty;
+        [ForeignKey("DebtorId")]
+        public User? Debtor { get; set; }
+        public DebtChargeStatus Status { get; set; }
+    }
+}
---
+++ ExpenseManagement.Api/Data/Models/TransactionHistory.cs
@@ -0,0 +1,23 @@
+﻿using ExpenseManagement.Api.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseManagement.Api.Data.Models
+{
+    public class TransactionHistory
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int DebtChargeId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string DebtorId { get; set; } = string.Empty;
+        public string CreditorId { get; set; } = string.Empty;
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Amount { get; set; }
+        public PaymentMethodCode? PaymentMethod { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public TransactionHistoryStatus Status { get; set; }
+    }
+}
