1805dd32b31b1f8704b6c54665890c72b3df754e
Truong Hoang <hqtruong27@gmail.com>
2022-03-02 13:07:26 +0700

    Global Exception cancelledtoken filter

---
+++ ExpenseManagement.Api/Attribute/OperationCanceledExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ExpenseManagement.Api.Attribute
+{
+    public class OperationCanceledExceptionFilter : ExceptionFilterAttribute
+    {
+        private readonly ILogger _logger;
+
+        public OperationCanceledExceptionFilter(ILogger<OperationCanceledExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception is OperationCanceledException)
+            {
+                _logger.LogInformation("Request was cancelled");
+                context.ExceptionHandled = true;
+                const int CLIENT_CLOSED_REQUEST = 499;
+                context.Result = new Microsoft.AspNetCore.Mvc.StatusCodeResult(CLIENT_CLOSED_REQUEST);
+            }
+        }
+    }
+}
