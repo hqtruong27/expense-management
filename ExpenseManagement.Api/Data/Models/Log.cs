b0d6f66184ed0747133b6b2b07f46ae91c246813
Truong Hoang <hqtruong27@gmail.com>
2022-03-03 22:48:33 +0700

    add table log

---
+++ ExpenseManagement.Api/Data/Models/Log.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseManagement.Api.Data.Models
+{
+    public class Log
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? Message { get; set; }
+        public string? MessageTemplate { get; set; }
+        public string? Level { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? TimeStamp { get; set; }
+        public string? Exception { get; set; }
+        public string? Properties { get; set; }
+    }
+}
