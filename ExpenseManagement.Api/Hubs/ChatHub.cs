47107a99bdd548b9c32ec85dd3c725ac550ca149
Truong Hoang <hqtruong27@gmail.com>
2022-03-03 22:48:50 +0700

    noti and cha realtime

---
+++ ExpenseManagement.Api/Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ExpenseManagement.Api.Hubs
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("connect", $"{Context.UserIdentifier} joined.");
+
+            await base.OnConnectedAsync();
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
--- ExpenseManagement.Api/Hubs/NotificationHub.cs
+++ ExpenseManagement.Api/Hubs/NotificationHub.cs
@@ -7,9 +7,20 @@ namespace ExpenseManagement.Api.Hubs
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class NotificationHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        private readonly ILogger<NotificationHub> _logger;
+
+        public NotificationHub(ILogger<NotificationHub> logger)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            _logger = logger;
+        }
+
+        public async Task Send(object message, params string[] userIds)
+        {
+            _logger.LogInformation("START: Send notify to users {userIds}", userIds);
+
+            await Clients.Users(userIds).SendAsync("ReceiveNotify", message);
+
+            _logger.LogInformation("END: success send notify to users");
         }
     }
 }
