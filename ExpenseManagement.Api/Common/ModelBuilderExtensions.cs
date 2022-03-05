e846f4ffae7e41c059b2873e7b010293ba45f9a3
Truong Hoang <hqtruong27@gmail.com>
2022-03-06 00:22:39 +0700

    modelbuilder

---
+++ ExpenseManagement.Api/Common/ModelBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using ExpenseManagement.Api.Data.Models;
+using ExpenseManagement.Api.Data.Models.Base;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ExpenseManagement.Api.Common
+{
+    public static class ModelBuilderExtensions
+    {
+        private static readonly MethodInfo _propertyMethod = typeof(EF).GetMethods(BindingFlags.Static | BindingFlags.Public)
+                                                                       .First(x => x.Name == nameof(EF.Property))
+                                                                       .MakeGenericMethod(typeof(bool));
+        public static void FilterDeletedRecords(this ModelBuilder modelBuilder)
+        {
+            foreach (var entity in from entity in modelBuilder.Model.GetEntityTypes()
+                                   where typeof(IPersistentEntity).IsAssignableFrom(entity.ClrType)
+                                   && !typeof(Role).IsAssignableFrom(entity.ClrType) //Exclude Role type
+                                   select entity)
+            {
+                modelBuilder.Entity(entity.ClrType).HasQueryFilter(GetIsDeletedRestriction(entity.ClrType));
+            }
+        }
+
+        private static LambdaExpression GetIsDeletedRestriction(Type type)
+        {
+            var param = Expression.Parameter(type, "it");
+            var prop = Expression.Call(_propertyMethod, param, Expression.Constant("IsDeleted"));
+            var condition = Expression.MakeBinary(ExpressionType.Equal, prop, Expression.Constant(false));
+            var lambda = Expression.Lambda(condition, param);
+            return lambda;
+        }
+    }
+}
