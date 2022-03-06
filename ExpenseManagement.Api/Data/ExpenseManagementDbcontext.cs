931bb6011e0fc0a675c4172a99f4021e1d91fd4a
Truong Hoang <hqtruong27@gmail.com>
2022-03-06 20:58:17 +0700

    new repo

--- ExpenseManagement.Api/Data/ExpenseManagementDbcontext.cs
+++ ExpenseManagement.Api/Data/ExpenseManagementDbcontext.cs
@@ -1,65 +1,46 @@
-﻿using ExpenseManagement.Api.Data.Models;
+﻿using ExpenseManagement.Api.Common;
+using ExpenseManagement.Api.Data.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 
 namespace ExpenseManagement.Api.Data
 {
     public class ExpenseManagementDbcontext : IdentityDbContext<User, Role, string>
     {
-        private static readonly MethodInfo _propertyMethod = typeof(EF).GetMethods(BindingFlags.Static | BindingFlags.Public)
-                                                                       .First(x => x.Name == nameof(EF.Property)).MakeGenericMethod(typeof(bool));
-
-        public ExpenseManagementDbcontext(DbContextOptions options) : base(options)
-        {
-        }
+        public ExpenseManagementDbcontext(DbContextOptions options) : base(options) { }
 
         public DbSet<Expense>? Expenses { get; set; }
         public DbSet<UserExpense>? UserExpenses { get; set; }
         public DbSet<DebtCharge>? DebtCharges { get; set; }
+        public DbSet<DebtReminder>? DebtReminders { get; set; }
         public DbSet<TransactionHistory>? TransactionHistories { get; set; }
         public DbSet<Log>? Logs { get; set; }
+        public DbSet<Chat>? Chats { get; set; }
+        public DbSet<UserChat>? UserChats { get; set; }
+        public DbSet<Message>? Messages { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<UserExpense>()
                    .HasKey(p => new { p.UserId, p.ExpenseId });
 
             builder.Entity<DebtCharge>()
-                .HasOne(s => s.Creditor)
-                .WithMany(u => u.Creditors)
-                .HasForeignKey(u => u.CreditorId)
-                .OnDelete(DeleteBehavior.Restrict);
+                   .HasOne(s => s.Creditor)
+                   .WithMany(u => u.Creditors)
+                   .HasForeignKey(u => u.CreditorId)
+                   .OnDelete(DeleteBehavior.Restrict);
 
             builder.Entity<DebtCharge>()
-                .HasOne(s => s.Debtor)
-                .WithMany(u => u.Debtors)
-                .HasForeignKey(u => u.DebtorId)
-                .OnDelete(DeleteBehavior.Restrict);
+                   .HasOne(s => s.Debtor)
+                   .WithMany(u => u.Debtors)
+                   .HasForeignKey(u => u.DebtorId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<UserChat>()
+                   .HasKey(x => new { x.UserId, x.ChatId });
+
+            builder.FilterDeletedRecords();
 
             base.OnModelCreating(builder);
-            FilterDeletedRecords(builder);
-        }
-
-        private void FilterDeletedRecords(ModelBuilder modelBuilder)
-        {
-            foreach (var entity in modelBuilder.Model.GetEntityTypes())
-            {
-                if (typeof(Models.Base.IPersistentEntity).IsAssignableFrom(entity.ClrType) && !typeof(User).IsAssignableFrom(entity.ClrType) && !typeof(Role).IsAssignableFrom(entity.ClrType)) //Exclude User type
-                {
-                    modelBuilder.Entity(entity.ClrType).HasQueryFilter(GetIsDeletedRestriction(entity.ClrType));
-                }
-            }
-        }
-
-        private static LambdaExpression GetIsDeletedRestriction(Type type)
-        {
-            var param = Expression.Parameter(type, "it");
-            var prop = Expression.Call(_propertyMethod, param, Expression.Constant("IsDeleted"));
-            var condition = Expression.MakeBinary(ExpressionType.Equal, prop, Expression.Constant(false));
-            var lambda = Expression.Lambda(condition, param);
-            return lambda;
         }
     }
 }
---
+++ ExpenseManagement.Api/Data/Repositories/DebtReminderRepository.cs
@@ -0,0 +1,15 @@
+﻿using ExpenseManagement.Api.Data.Models;
+using ExpenseManagement.Api.Data.Repositories.Generic;
+
+namespace ExpenseManagement.Api.Data.Repositories
+{
+    public interface IDebtReminderRepository : IRepository<DebtReminder>
+    {
+    }
+    public class DebtReminderRepository : GenericRepository<DebtReminder>, IDebtReminderRepository
+    {
+        public DebtReminderRepository(ExpenseManagementDbcontext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
