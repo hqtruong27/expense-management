d186cf675624706a1697fade7cbedb9a116c8c3c
Truong Hoang <hqtruong27@gmail.com>
2022-03-14 16:16:05 +0700

    mapping extension

--- ExpenseManagement.Api/Infrastructure/mapper/ServiceProfile.cs
+++ ExpenseManagement.Api/Infrastructure/mapper/ServiceProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using ExpenseManagement.Api.Data.Models;
 using ExpenseManagement.Api.Model;
+using System.Linq.Expressions;
 
 namespace ExpenseManagement.Api.Infrastructure.mapper
 {
@@ -19,4 +20,14 @@ namespace ExpenseManagement.Api.Infrastructure.mapper
             CreateMap<UserExpense, UserExpenseResponse>();
         }
     }
+
+    public static class MappingExtensions
+    {
+        public static IMappingExpression<TSource, TDestination> MapFrom<TSource, TDestination, TMember, TSourceMember>(
+            this IMappingExpression<TSource, TDestination> mapping,
+            Expression<Func<TDestination, TMember>> destinationMember,
+            Expression<Func<TSource, TSourceMember>> sourceMember)
+            => mapping.ForMember(destinationMember, x => x.MapFrom(sourceMember));
+    }
+
 }
