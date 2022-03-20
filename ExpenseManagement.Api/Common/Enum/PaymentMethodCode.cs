80bf4729b11a961f691e194925751f7ce2418216
Truong Hoang <hqtruong27@gmail.com>
2022-03-20 22:42:15 +0700

    add display nane

--- ExpenseManagement.Api/Common/Enum/PaymentMethodCode.cs
+++ ExpenseManagement.Api/Common/Enum/PaymentMethodCode.cs
@@ -1,8 +1,12 @@
-﻿namespace ExpenseManagement.Api.Enum
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseManagement.Api.Enum
 {
     public enum PaymentMethodCode
     {
+        [Display(Name = "Tiền mặt")]
         Cash,
+        [Display(Name = "Thẻ tín dụng")]
         Credit
     }
 }
