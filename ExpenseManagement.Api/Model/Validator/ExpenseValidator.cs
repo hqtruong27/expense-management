31d9c2892da10938af68aa89fe6a3eeb6861dbe6
Truong Hoang <hqtruong27@gmail.com>
2022-03-20 22:43:47 +0700

    expense fluent api validator

---
+++ ExpenseManagement.Api/Model/Validator/ExpenseValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace ExpenseManagement.Api.Model.Validator
+{
+    public class ExpenseCreateRequestValidator : AbstractValidator<ExpenseCreateRequest>
+    {
+        public ExpenseCreateRequestValidator()
+        {
+            Include(new ExpenseUpdateRequestValidator());
+        }
+    }
+
+    public class ExpenseUpdateRequestValidator : AbstractValidator<ExpenseUpdateRequest>
+    {
+        public ExpenseUpdateRequestValidator()
+        {
+            RuleFor(x => x.Name).Cascade(CascadeMode.Stop).NotEmpty();
+            RuleFor(x => x.Amount).Must(amount => amount > 0).WithMessage("Amount must be greater than 0");
+        }
+    }
+}
