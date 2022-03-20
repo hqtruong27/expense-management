8e76c6f8d42e10d8e8b903d6caccb7db88ef5f1d
Truong Hoang <hqtruong27@gmail.com>
2022-03-20 22:43:18 +0700

    Test

---
+++ ExpenseManagement.Api/Hubs/LogHub.cs
@@ -0,0 +1,28 @@
+﻿using ExpenseManagement.Api.Data;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseManagement.Api.Hubs
+{
+    public class LogHub : Hub
+    {
+        private readonly ExpenseManagementDbcontext _context;
+
+        private static CancellationToken CancellationToken => CancellationToken.None;
+        public LogHub(ExpenseManagementDbcontext context)
+        {
+            _context = context;
+        }
+
+        public async Task SendLogs()
+        {
+            var data = _context.Logs;
+            if (data != null)
+            {
+                var logs = await data.ToListAsync();
+                await Clients.All.SendAsync("logs", logs, CancellationToken);
+
+            }
+        }
+    }
+}
