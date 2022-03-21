234ee8480c4b9f0aba878e1e0363c847fdbc1506
Truong Hoang <hqtruong27@gmail.com>
2022-03-21 20:57:33 +0700

    sm change, deconstuc extension

---
+++ ExpenseManagement.Api/Common/DeconstructExtensions.cs
@@ -0,0 +1,54 @@
+﻿namespace ExpenseManagement.Api.Common
+{
+    public static class DateTimeDeconstructExtensions
+    {
+
+        public static void Deconstruct(this DateOnly source, out int year, out int month, out int day)
+        {
+
+            year = source.Year;
+            month = source.Month;
+            day = source.Day;
+        }
+
+        public static void Deconstruct(this DateTime source, out int year, out int month, out int day)
+        {
+            year = source.Year;
+            month = source.Month;
+            day = source.Day;
+        }
+
+        public static void Deconstruct(this DateTimeOffset source, out int year, out int month, out int day)
+        {
+            year = source.Year;
+            month = source.Month;
+            day = source.Day;
+        }
+
+        public static void Deconstruct(this DateTimeOffset source, out int year, out int month, out int day, out int hour, out int minute, out int second)
+        {
+            year = source.Year;
+            month = source.Month;
+            day = source.Day;
+            hour = source.Hour;
+            minute = source.Minute;
+            second = source.Second;
+        }
+
+        public static void Deconstruct(this DateTime source, out int year, out int month, out int day, out int hour, out int minute, out int second)
+        {
+            year = source.Year;
+            month = source.Month;
+            day = source.Day;
+            hour = source.Hour;
+            minute = source.Minute;
+            second = source.Second;
+        }
+    }
+
+    public static class DeconstructExtensions
+    {
+
+
+    }
+}
--- ExpenseManagement.Api/Common/Options/AuthenticationOptions.cs
+++ ExpenseManagement.Api/Common/Options/AuthenticationOptions.cs
@@ -4,50 +4,31 @@ namespace ExpenseManagement.Api.Options
 {
     public class Authentication
     {
-        public Authentication()
-        {
-            Google = new OAuthOptions();
-            Facebook = new FacebookOptions();
-            JWT = new JwtOptions();
-            Twilio = new TwilioOptions();
-        }
 
-        public OAuthOptions Google { get; set; }
-        public FacebookOptions Facebook { get; set; }
-        public JwtOptions JWT { get; set; }
-        public TwilioOptions Twilio { get; set; }
+        public OAuthOptions Google { get; set; } = default!;
+        public FacebookOptions Facebook { get; set; } = default!;
+        public JwtOptions JWT { get; set; } = default!;
+        public TwilioOptions Twilio { get; set; } = default!;
     }
 
     public class JwtOptions
     {
         public string? ValidAudience { get; set; }
         public string? ValidIssuer { get; set; }
-        public string Secret { get; set; }
+        public string Secret { get; set; } = default!;
         /// <summary>
         /// Token Expiration Date ex: 5 = 5days
         /// </summary>
         public int Expired { get; set; }
-        public JwtOptions()
-        {
-            Secret = string.Empty;
-        }
+
     }
 
     public class FacebookOptions
     {
-        public FacebookOptions()
-        {
-            TokenValidateUri = string.Empty;
-            UserInfoUri = string.Empty;
-            CliendId = string.Empty;
-            AppId = string.Empty;
-            AppSecrect = string.Empty;
-        }
-
-        public string TokenValidateUri { get; set; }
-        public string UserInfoUri { get; set; }
-        public string CliendId { get; set; }
-        public string AppId { get; set; }
-        public string AppSecrect { get; set; }
+        public string TokenValidateUri { get; set; } = default!;
+        public string UserInfoUri { get; set; } = default!;
+        public string CliendId { get; set; } = default!;
+        public string AppId { get; set; } = default!;
+        public string AppSecrect { get; set; } = default!;
     }
 }
