6c6cdcc650c7d39480edc1ede6abdbdacc49757a
Truong Hoang <hqtruong27@gmail.com>
2022-03-23 20:05:52 +0700

    global using

---
+++ ExpenseManagement.Api/Start/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using ExpenseManagement.Api.Models;
+global using ExpenseManagement.Api.Data.Repositories;
+global using ExpenseManagement.Api.Common;
+global using ExpenseManagement.Api.Enum;
+global using ExpenseManagement.Api.Common.Resources;
